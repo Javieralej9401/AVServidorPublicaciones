fbe9562b94cacbd37edc46dd420945a987654171
EJC Software Solutions <ejcsoftwaresolutions@gmail.com>
2017-01-03 17:05:05 -0430

    changes

--- app.js
+++ app.js
@@ -21,14 +21,14 @@ var multipart = require('connect-multiparty');
  mysql= require("./controladores/conexionBD");
  creds = require("./controladores/conexionBD/db_creds.json");
  con =  {};
-
+/*
 creds = {
   "host":  "localhost", 
   "user":  "root" , 
   "pass":  "", 
   "db": "aulavirtbd" , 
   "port": 3306
-}
+}*/
 try{
   
   con= mysql.nueva_conexion_bd(creds.host,  creds.user, creds.pass, creds.db, creds.port);
